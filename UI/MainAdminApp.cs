540ef080957b96c56a705cf7a6aa06bca9b932f1
resnakk <masanchez3@miuandes.cl>
2018-05-10 21:46:59 -0300

    MainAdminApp.cs

---
+++ UI/MainAdminApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI
+{
+    public partial class MainAdminApp : Form
+    {
+        public MainAdminApp()
+        {
+            InitializeComponent();
+        }
+
+        private void BExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BRemoveUser_Click(object sender, EventArgs e)
+        {
+            string Mail = TMail.Text;
+        }
+
+        private void BRemoveStores_Click(object sender, EventArgs e)
+        {
+            string Local = CStore.SelectedItem.ToString();
+        }
+    }
+}
